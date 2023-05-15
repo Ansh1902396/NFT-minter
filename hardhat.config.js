cda8cb811901e4fe34439d8c69bc922f6b8df1db
Rudransh Shinghal <rudransh_shinghal@yahoo.co.in>
2023-05-15 15:56:50 +0000

    files changed

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    polygon_mumbai: {
+    mumbai: {
       url: "https://capable-still-knowledge.matic-testnet.discover.quiknode.pro/57a6ed7c71e095a27ce83b5c938e3b7e76aa1279/",
       accounts: ["52cc9c933861890944ee0b759059e5d364787c5cef63cb34153fc73de8daaf4e"],
     },
