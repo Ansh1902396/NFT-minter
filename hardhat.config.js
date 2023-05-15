7ce45853f951550b14c0fd864427dc71b9f025e1
Ansh1902396 <84223519+Ansh1902396@users.noreply.github.com>
2023-05-15 21:04:43 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    polygon_mumbai: {
+      url: "https://capable-still-knowledge.matic-testnet.discover.quiknode.pro/57a6ed7c71e095a27ce83b5c938e3b7e76aa1279/",
+      accounts: ["52cc9c933861890944ee0b759059e5d364787c5cef63cb34153fc73de8daaf4e"],
+    },
+  },
 };
